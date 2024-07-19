ba02e75b6da1df80d893258fe7322ef86896e509
p0lyMth <michaelsodeke@outlook.com>
2024-07-19 11:51:34 -0700

    master->[add] code to respond to HTTP requests with a 200 status code.

--- app/main.js
+++ app/main.js
@@ -3,10 +3,10 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+const server = net.createServer(
+(socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("close", () => { socket.end(); });
 });
 
 server.listen(4221, "localhost");
