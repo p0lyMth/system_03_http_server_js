e8b1544a73ca8fe320fb56605e30d787c45282e0
p0lyMth <michaelsodeke@outlook.com>
2024-07-20 09:39:15 -0700

    master->[add] HTTP GET request handler to extract and respond with status code.

--- app/main.js
+++ app/main.js
@@ -1,11 +1,23 @@
-const net = require("net");
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const net     = require("net");
 
 const server = net.createServer(
 (socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  // -- GET request handler
+  socket.on("data", (data) => {
+    console.log(`Incoming request:\n${data.toString()}`);
+
+    const req  = data.toString().split('\r\n')[0];
+    rgx_origin = /^GET\s\/\sHTTP\/1\.1$/g;
+    if (rgx_origin.test(req)) {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
+    socket.end();
+  });  
+
+  // -- close handler
   socket.on("close", () => { socket.end(); });
 });
 
