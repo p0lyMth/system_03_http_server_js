aac8d1acb6401b49f3f43b19cd5e1a0acf431411
p0lyMth <michaelsodeke@outlook.com>
2024-07-20 10:52:29 -0700

    master->[add] echo API/endpoint to accept and send strings to response body.

--- app/main.js
+++ app/main.js
@@ -1,25 +1,34 @@
 const net     = require("net");
 
+// -- GET request handler
+function GET(req, socket) {
+  rgx_origin_root = /^GET\s\/\sHTTP\/1\.1$/;
+  rgx_origin_echo = /^GET\s\/echo\/([^\s?]*)\sHTTP\/1\.1$/;
+
+  if (rgx_origin_root.test(req)) {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  } else if (rgx_origin_echo.test(req)) {
+    const resp = rgx_origin_echo.exec(req);
+    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${Buffer.byteLength(resp[1])}\r\n\r\n${resp[1]}`);
+  } else {
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+  }
+}
+
+// -- create server
 const server = net.createServer(
 (socket) => {
   // -- GET request handler
   socket.on("data", (data) => {
     console.log(`Incoming request:\n${data.toString()}`);
-
     const req  = data.toString().split('\r\n')[0];
 
-    rgx_origin = /^GET\s\/\sHTTP\/1\.1$/g;
-    if (rgx_origin.test(req)) {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      socket.end();
-      return;
-    }
+    GET(req, socket);
 
-    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     socket.end();
   });  
 
-  // -- close handler
+  // -- connection close handler
   socket.on("close", () => { socket.end(); });
 });
 
